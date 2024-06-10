1702537dfe05f60760a470329547913db759b945
vaibhavdachawar <123737345+vaibhavdachawar@users.noreply.github.com>
2024-06-10 21:55:12 +0530

    I have done changes inside the test1 and test2 class

---
+++ src/test/java/githubProject_Eve/Test.java
@@ -0,0 +1,10 @@
+package githubProject_Eve;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is test class");
+	}
+
+}
---
+++ src/test/java/githubProject_Eve/Test2.java
@@ -0,0 +1,10 @@
+package githubProject_Eve;
+
+public class Test2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is test2 class ");
+	}
+
+}
